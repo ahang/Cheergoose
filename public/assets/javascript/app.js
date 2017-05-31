ff43104b073d0bf4e7f7ffebd7a2df4694e05808
ahang <hang.andy.t@gmail.com>
2017-05-31 13:18:24 -0500

    modal work in progress. not launching..

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -15,6 +15,26 @@ function getData() {
                     </td>
                 </tr>`
             );
+        var modals = (`
+            <div class="modal" id="#${data[i]._id}" role="dialog">
+                <div class="modal-dialog">
+
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Modal Header</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Some text in the modal.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `)
+        $(".modals").append(modals);
         }
     });
 }
